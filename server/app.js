d1ad64caf453a025312610787bbd4b9684d2690a
JudeEdemNyahe <97879372+JudeEdemNyahe@users.noreply.github.com>
2022-12-06 18:34:34 +0000

    related schema data

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
+const schema = require("./schema/schema");
 
 const app = express();
 const PORT = 4000;
 
 //fires when ever our endpoint starts with 'graphql'
-app.use("/graphql", graphqlHTTP({}));
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+    })
+);
 
 app.listen(PORT, () => {
     console.log(`Server listening for request on port ${PORT}`);
